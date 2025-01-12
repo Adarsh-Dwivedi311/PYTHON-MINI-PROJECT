a5a963836e73a4cdd2871582de02e0e1ad521091
Adarsh-Dwivedi311 <adarshdubeyadarshdwivedi@gmail.com>
2025-01-12 15:15:54 +0530

    Add files via upload

---
+++ Alexa.py
@@ -0,0 +1,52 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+
+listener=sr.Recognizer()
+engine=pyttsx3.init()
+voices=engine.getProperty("voices")
+engine.setProperty("voice" ,voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def alexa_command():
+    try:
+     with sr.Microphone() as source:
+        print("Lisnenig...")
+        voice=listener.listen(source)
+        command=listener.recognize_google(voice)
+        command=command.lower()
+        if"alexa" in command:
+            command=command.replace("alexa","")
+            talk(command)
+             
+    except:
+       pass
+    return command
+
+def run_alexa():
+    command=alexa_command()
+    print(command)
+    if "play" in command:
+        song=command.replace("play","")
+        talk("Playing" +song)
+        pywhatkit.playonyt(song)
+    elif "time" in command:
+        time=datetime.datetime.now().strftime("%H:%M")
+        print(time)
+        talk("night time is"+time)
+    elif "scientist" in command:
+        person=command.replace("scientist","")
+        info=wikipedia.summary(person,4)
+        print(info)
+        talk(info)  
+        
+
+run_alexa()
+
+
+
